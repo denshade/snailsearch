07be5df5ed211520b226e06efb0bf1e0affd09b8
denshade <lieven.vaneeckhaute@gmail.com>
2025-10-30 14:24:58 +0100

    fix website

--- search_db.py
+++ search_db.py
@@ -1,8 +1,11 @@
 import sqlite3
 
+
 def must_contain(words: list):
     filter = ""
     for word in words:
+        if word == "":
+            continue
         filter = filter + f" AND text LIKE '%,{word.lower()},%'"
     length = len(" AND ")
     filter = filter[length:]
@@ -18,6 +21,8 @@ def print_results(results):
 def must_contain_any(words: list):
     filter = ""
     for word in words:
+        if word == "":
+            continue
         filter = filter + f" OR text LIKE '%,{word.lower()},%'"
     length = len(" OR ")
     filter = filter[length:]
@@ -27,10 +32,13 @@ def must_contain_any(words: list):
 def must_not_contain_all(words: list):
     filter = ""
     for word in words:
+        if word == "":
+            continue
         filter = filter + f" AND text NOT LIKE '%,{word.lower()},%'"
     length = len(" AND ")
     filter = filter[length:]
     return filter
+
 
 def do_filter(cur, or_word_list, and_word_list, not_words):
     and_list = must_contain(and_word_list)
@@ -45,13 +53,17 @@ def do_filter(cur, or_word_list, and_word_list, not_words):
         filters.append(not_list)
 
     filter = " AND ".join(filters)
-    query = f"SELECT URL from site where {filter}"
+    if filter == "":
+        query = "SELECT URL from site"
+    else:
+        query = f"SELECT URL from site where {filter}"
     res = cur.execute(query)
     results = res.fetchall()
-    print_results(results)
+    return results
 
 
-con = sqlite3.connect("data/websites.db")
-cur = con.cursor()
+def search_on_host(or_list, and_list, not_list):
+    con = sqlite3.connect("data/websites.db")
+    cur = con.cursor()
 
-do_filter(cur, ["Putin", "Poetin"], [], ["Trump"])
+    return do_filter(cur, or_list, and_list, not_list)
---
+++ search_server.py
@@ -0,0 +1,32 @@
+# Importing flask module in the project is mandatory
+# An object of Flask class is our WSGI application.
+from flask import Flask, request, render_template
+
+from search_db import search_on_host
+
+# Flask constructor takes the name of
+# current module (__name__) as argument.
+app = Flask(__name__)
+
+
+# The route() function of the Flask class is a decorator,
+# which tells the application which URL should call
+# the associated function.
+@app.route('/search', methods=['POST'])
+def search():
+    and_list = request.form['andlist'].split(" ")
+    or_list = request.form['orlist'].split(" ")
+    not_list = request.form['notlist'].split(" ")
+    urls = search_on_host(or_list, and_list, not_list)
+    return render_template('results.html', urls=urls)
+
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+# main driver function
+if __name__ == '__main__':
+    # run() method of Flask class runs the application
+    # on the local development server.
+    app.run()
